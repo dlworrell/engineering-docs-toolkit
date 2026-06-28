998424886877649a28ef9a75eabe5351b94099de
Donovan Worrell <donovan.worrell@just-a-geek.com>
2026-06-28 15:39:57 -0700

    Expand EDOM JSON dict tests

--- tests/test_edom_json.py
+++ tests/test_edom_json.py
@@ -2,6 +2,11 @@ from edt.edom import EdomNode
 from edt.edom_json import node_to_dict
 
 
-def test_node_to_dict():
+def test_node_to_dict_kind():
     data = node_to_dict(EdomNode(kind="document"))
     assert data["kind"] == "document"
+
+
+def test_node_to_dict_fingerprint():
+    data = node_to_dict(EdomNode(kind="document"))
+    assert "fingerprint" in data
