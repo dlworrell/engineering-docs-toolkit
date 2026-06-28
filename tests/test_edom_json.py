e763d5db35400b0c854479a4489d5aea22814a41
Donovan Worrell <donovan.worrell@just-a-geek.com>
2026-06-28 15:40:14 -0700

    Add EDOM JSON child test

--- tests/test_edom_json.py
+++ tests/test_edom_json.py
@@ -10,3 +10,10 @@ def test_node_to_dict_kind():
 def test_node_to_dict_fingerprint():
     data = node_to_dict(EdomNode(kind="document"))
     assert "fingerprint" in data
+
+
+def test_node_to_dict_children():
+    node = EdomNode(kind="document")
+    node.add(EdomNode(kind="paragraph"))
+    data = node_to_dict(node)
+    assert len(data["children"]) == 1
