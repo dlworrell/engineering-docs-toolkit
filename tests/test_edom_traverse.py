0936ba48c61eed9d6855ff16124c8275d3bd2dd2
Donovan Worrell <donovan.worrell@just-a-geek.com>
2026-06-28 16:10:21 -0700

    Test EDOM postorder traversal

--- tests/test_edom_traverse.py
+++ tests/test_edom_traverse.py
@@ -1,11 +1,17 @@
 from edt.edom import EdomNode
-from edt.edom_traverse import find_by_kind, preorder
+from edt.edom_traverse import find_by_kind, postorder, preorder
 
 
 def test_preorder():
     node = EdomNode(kind="document")
     node.add(EdomNode(kind="paragraph"))
     assert [item.kind for item in preorder(node)] == ["document", "paragraph"]
+
+
+def test_postorder():
+    node = EdomNode(kind="document")
+    node.add(EdomNode(kind="paragraph"))
+    assert [item.kind for item in postorder(node)] == ["paragraph", "document"]
 
 
 def test_find_by_kind():
