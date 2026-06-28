c1290b35b49b68cee096dea2be747b2eb5576596
Donovan Worrell <donovan.worrell@just-a-geek.com>
2026-06-28 14:22:20 -0700

    Add init subcommand to CLI

--- edt/cli.py
+++ edt/cli.py
@@ -1,15 +1,20 @@
 import argparse
+from pathlib import Path
 
 from .build import build_project
+from .init_project import init_project
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(prog="edt")
     sub = parser.add_subparsers(dest="command")
     sub.add_parser("build")
+    sub.add_parser("init")
     args = parser.parse_args()
     if args.command in (None, "build"):
         build_project()
+    elif args.command == "init":
+        init_project(Path.cwd())
 
 
 if __name__ == "__main__":
