ff5a79b172edb49bb22325036e32f2065f31235e
Donovan Worrell <donovan.worrell@just-a-geek.com>
2026-06-28 14:21:57 -0700

    Add build subcommand to CLI

--- edt/cli.py
+++ edt/cli.py
@@ -1,8 +1,15 @@
+import argparse
+
 from .build import build_project
 
 
 def main() -> None:
-    build_project()
+    parser = argparse.ArgumentParser(prog="edt")
+    sub = parser.add_subparsers(dest="command")
+    sub.add_parser("build")
+    args = parser.parse_args()
+    if args.command in (None, "build"):
+        build_project()
 
 
 if __name__ == "__main__":
