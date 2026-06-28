ac7dfbe63f2e0d562c27c70daad73fdbb926516e
Donovan Worrell <donovan.worrell@just-a-geek.com>
2026-06-28 14:24:14 -0700

    Add check subcommand to CLI

--- edt/cli.py
+++ edt/cli.py
@@ -2,6 +2,7 @@ import argparse
 from pathlib import Path
 
 from .build import build_project
+from .check import check_project
 from .init_project import init_project
 
 
@@ -9,10 +10,16 @@ def main() -> None:
     parser = argparse.ArgumentParser(prog="edt")
     sub = parser.add_subparsers(dest="command")
     sub.add_parser("build")
+    sub.add_parser("check")
     sub.add_parser("init")
     args = parser.parse_args()
     if args.command in (None, "build"):
         build_project()
+    elif args.command == "check":
+        issues = check_project(Path.cwd())
+        for issue in issues:
+            print(issue)
+        raise SystemExit(1 if issues else 0)
     elif args.command == "init":
         init_project(Path.cwd())
 
