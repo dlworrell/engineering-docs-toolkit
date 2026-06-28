a59e7df5ae2bc97ef10250c26b1b01109cb7b0bc
Donovan Worrell <donovan.worrell@just-a-geek.com>
2026-06-28 16:09:38 -0700

    Add EDOM postorder traversal

--- edt/edom_traverse.py
+++ edt/edom_traverse.py
@@ -9,5 +9,11 @@ def preorder(node: EdomNode) -> Iterator[EdomNode]:
         yield from preorder(child)
 
 
+def postorder(node: EdomNode) -> Iterator[EdomNode]:
+    for child in node.children:
+        yield from postorder(child)
+    yield node
+
+
 def find_by_kind(node: EdomNode, kind: str) -> list[EdomNode]:
     return [item for item in preorder(node) if item.kind == kind]
