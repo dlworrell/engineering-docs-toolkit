8e4f382efd42bcaaf6663dc3e7b2704f671e216f
Donovan Worrell <donovan.worrell@just-a-geek.com>
2026-06-29 11:59:42 -0700

    Test semantic block metadata default

--- tests/test_semantic_blocks.py
+++ tests/test_semantic_blocks.py
@@ -1,5 +1,5 @@
 from edt.layout_model import LayoutBlock
-from edt.semantic_blocks import semantic_block_from_layout
+from edt.semantic_blocks import SemanticBlock, semantic_block_from_layout
 
 
 def test_semantic_block_from_layout_preserves_text():
@@ -7,3 +7,8 @@ def test_semantic_block_from_layout_preserves_text():
     semantic = semantic_block_from_layout(block, "definition")
     assert semantic.semantic_kind == "definition"
     assert semantic.text == block.text
+
+
+def test_semantic_block_metadata_default():
+    block = SemanticBlock(block_id="b1", semantic_kind="equation", text="x = 1")
+    assert block.metadata == {}
