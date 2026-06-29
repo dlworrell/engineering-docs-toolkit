63b50de2863a366601edb34942f60a97c1e125a0
Donovan Worrell <donovan.worrell@just-a-geek.com>
2026-06-29 08:55:15 -0700

    Test plugin cache key

--- tests/test_plugin_cache.py
+++ tests/test_plugin_cache.py
@@ -1,2 +1,6 @@
-def test_plugin_cache_placeholder():
-    assert True
+from edt.plugin_cache import cache_key, cached_execute
+
+
+def test_cache_key_contains_plugin_id():
+    key = cache_key("plugin-a", "node-a", "hash-a")
+    assert key.startswith("plugin-a:")
