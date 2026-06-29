13541a32ed0d3b8c2c832f4d47704dbcf48f8927
Donovan Worrell <donovan.worrell@just-a-geek.com>
2026-06-29 11:28:30 -0700

    Test code quote and citation recognition

--- tests/test_semantic_recognizers.py
+++ tests/test_semantic_recognizers.py
@@ -40,3 +40,18 @@ def test_recognize_title_kind():
 def test_recognize_author_kind():
     block = LayoutBlock(block_id="author", kind="author", text="A. Author")
     assert recognize_semantic_kind(block) == "author"
+
+
+def test_recognize_code_listing_kind():
+    block = LayoutBlock(block_id="code1", kind="code", text="for i in range(3):")
+    assert recognize_semantic_kind(block) == "code_listing"
+
+
+def test_recognize_quotation_kind():
+    block = LayoutBlock(block_id="quote1", kind="quote", text="Quoted text")
+    assert recognize_semantic_kind(block) == "quotation"
+
+
+def test_recognize_citation_prefix():
+    block = LayoutBlock(block_id="cite1", kind="paragraph", text="Citation. Smith 2020")
+    assert recognize_semantic_kind(block) == "citation"
