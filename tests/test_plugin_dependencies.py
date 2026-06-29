7cb64d9ecc12d621721ab62a1e1c507daad50c3f
Donovan Worrell <donovan.worrell@just-a-geek.com>
2026-06-29 08:54:16 -0700

    Test plugin dependency set

--- tests/test_plugin_dependencies.py
+++ tests/test_plugin_dependencies.py
@@ -1,2 +1,8 @@
-def test_plugin_dependency_placeholder():
-    assert True
+from edt.plugin_dependencies import PluginDependencySet
+
+
+def test_plugin_dependency_set_records_input():
+    deps = PluginDependencySet(plugin_id="plugin-a")
+    deps.add_input("node-a")
+    result = deps.inputs
+    assert "node-a" in result
