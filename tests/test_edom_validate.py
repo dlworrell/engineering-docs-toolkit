ebd6e36e2a13731678412873f060709687fbf6ea
Donovan Worrell <donovan.worrell@just-a-geek.com>
2026-06-28 17:03:45 -0700

    Test EDOM heading level helper

--- tests/test_edom_validate.py
+++ tests/test_edom_validate.py
@@ -1,5 +1,5 @@
 from edt.edom import EdomNode
-from edt.edom_validate import duplicate_ids
+from edt.edom_validate import duplicate_ids, heading_level
 
 
 def test_duplicate_ids_empty():
@@ -11,3 +11,7 @@ def test_duplicate_ids_detected():
     root = EdomNode(kind="document", node_id="same")
     root.add(EdomNode(kind="paragraph", node_id="same"))
     assert duplicate_ids(root) == ["same"]
+
+
+def test_heading_level():
+    assert heading_level("heading2") == 2
