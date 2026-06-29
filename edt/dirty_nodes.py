5f3062dfc58a87d1e6cb8562c4910f1ad3ddaecb
Donovan Worrell <donovan.worrell@just-a-geek.com>
2026-06-29 08:52:36 -0700

    Add dirty EDOM node detector

--- edt/dirty_nodes.py
+++ edt/dirty_nodes.py
@@ -4,3 +4,7 @@ from .edom_traverse import preorder
 
 def fingerprint_map(root: EdomNode) -> dict[str, str]:
     return {node.node_id: node.fingerprint for node in preorder(root)}
+
+
+def dirty_node_ids(previous: dict[str, str], current: dict[str, str]) -> set[str]:
+    return {node_id for node_id, fingerprint in current.items() if previous.get(node_id) != fingerprint}
