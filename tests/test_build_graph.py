e3708ac9a1c543d663d5ca2bccb6805ca6fcb291
Donovan Worrell <donovan.worrell@just-a-geek.com>
2026-06-29 08:50:06 -0700

    Test build graph node registration

--- tests/test_build_graph.py
+++ tests/test_build_graph.py
@@ -1,2 +1,8 @@
-def test_build_graph_placeholder():
-    assert True
+from edt.build_graph import BuildGraph
+
+
+def test_build_graph_starts_empty():
+    build_graph = BuildGraph()
+    build_graph.add_node("node-a")
+    result = build_graph.upstream_of("node-a")
+    assert len(result) == 0
